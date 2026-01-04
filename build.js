a2aa5c08bb24a49fcf84697d3e11650114c4d7f1
sirjmann92 <sirjmann92@gmail.com>
2026-01-04 10:49:01 -0600

    Fix plugin installation - add required folder structure to zip

--- build.js
+++ build.js
@@ -17,20 +17,23 @@ archive.on('error', (err) => {
 
 archive.pipe(output);
 
-// Add all necessary files
-archive.file('manifest.json', { name: 'manifest.json' });
-archive.file('package.json', { name: 'package.json' });
-archive.directory('bin/', 'bin');
-archive.directory('propertyinspector/', 'propertyinspector');
+// Add all files with the plugin folder name as prefix
+// Stream Deck expects: pluginName.streamDeckPlugin/pluginName/manifest.json
+const prefix = pluginName + '/';
+
+archive.file('manifest.json', { name: prefix + 'manifest.json' });
+archive.file('package.json', { name: prefix + 'package.json' });
+archive.directory('bin/', prefix + 'bin');
+archive.directory('propertyinspector/', prefix + 'propertyinspector');
 
 // Add images if they exist
 if (fs.existsSync('images')) {
-  archive.directory('images/', 'images');
+  archive.directory('images/', prefix + 'images');
 }
 
 // Add node_modules only if they exist
 if (fs.existsSync('node_modules')) {
-  archive.directory('node_modules/', 'node_modules');
+  archive.directory('node_modules/', prefix + 'node_modules');
 }
 
 archive.finalize();
